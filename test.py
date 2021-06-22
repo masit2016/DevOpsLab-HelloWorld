dbf16b4872570678787536f46b0be8b34b47dfa6
matheus.ads <matheus.ads@gmail.com>
2021-06-22 13:30:17 -0400

    Atualização dos arquivos index.html

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Iniciando DEVOPS")
+        self.assertRegex(result.data.decode(), "TESTE TESTANDO")
 
 
 if __name__ == "__main__":
